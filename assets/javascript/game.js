b4084aa716078f08823d75a22ecf9f8f242efc9e
Brett Landes <brettl0407@gmail.com>
2020-02-24 17:35:39 -0500

    added pseudocode to javascript file

---
+++ assets/javascript/game.js
@@ -0,0 +1,91 @@
+$(document).ready(function() { 
+
+//define a variable for wins
+
+//define a variable for losses
+
+//define a variable set to 0 to add to when a user clicks an image
+
+//add click events to photos to add to users current score variable 
+
+//assign each image a random value of 1-12
+
+//get a random number 19-120
+
+//print random number to index.html page under random number span
+
+//compare current score variable to random number variable
+
+//if current score = random number, add 1 to win variable, alert "you won, keep going"
+
+//if current score > random number, add 1 to loss variable alert "you lost, try again"
+
+//restart game, keeping wins and loss variables the same, while shuffling image click values
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
