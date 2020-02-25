f74fde91384c633a7a7aad3fb768f8aec6a3046b
Brett Landes <brettl0407@gmail.com>
2020-02-25 18:08:43 -0500

    added a random number generator to set the goalNumber variable and .text it to the html page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,16 +6,74 @@ var wins="";
 var losses="";
 //define a variable set to 0 to add to when a user clicks an image
 var currentScore= 0;
+
+//COME BACK AND MAKE THIS CODE DRY===============================================================================
+//assign each image a random value of 1-12
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+var pictureValues = numbers[Math.floor(Math.random() * numbers.length)]
+
+$("#crystal-button-1").attr("value", pictureValues);
+
+
+
+// image 2 ------------------------------------------
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+var pictureValues = numbers[Math.floor(Math.random() * numbers.length)]
+
+$("#crystal-button-2").attr("value", pictureValues);
+
+
+
+//image 3 --------------------------------------------------
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+var pictureValues = numbers[Math.floor(Math.random() * numbers.length)]
+
+$("#crystal-button-3").attr("value", pictureValues);
+
+
+// image 4 -------------------------------------------------
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+var pictureValues = numbers[Math.floor(Math.random() * numbers.length)]
+
+$("#crystal-button-4").attr("value", pictureValues);
+
+
+
+
+//get a random number 19-120 ---------------------------------------
+var min = 19;
+
+var max = 120;
+
+var goalNumber = Math.floor(Math.random() * (+max - +min) + +min);
+
+$("#random-number").text(goalNumber);
+
+
+
+
+
+
 //add click events to photos to add to users current score variable 
+
 $(".crystal-button").on("click", function() {
     buttonValue = $(this).val();
     buttonValue = parseInt(buttonValue);
     currentScore = buttonValue + currentScore;
     $("#current-total").text(currentScore);
 });
-//assign each image a random value of 1-12
 
-//get a random number 19-120
+
+
+
+
+
+
+
 
 //print random number to index.html page under random number span
 
